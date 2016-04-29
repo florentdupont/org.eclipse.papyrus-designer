3e0fc10e96d2c8125dc87aa6e2eece7f25c0973b
Cedric Dumoulin <cedric.dumoulin@lifl.fr>
2016-04-30 00:55:11 +0200

    Bug 492754 -[designer][java][generator] Reenable code for the original Java Generator
    Reintroduce the original Java Profile from papyrus/extra/java. This
    reenable the original java generator.

---
+++ languages/java/org.eclipse.papyrus.designer.languages.java.profile/src/org/eclipse/papyrus/designer/languages/java/profile/java/util/JavaResourceFactoryImpl.java
@@ -0,0 +1,43 @@
+/**
+ */
+package org.eclipse.papyrus.designer.languages.java.profile.java.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+
+import org.eclipse.emf.ecore.xmi.XMLResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource Factory</b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.papyrus.designer.languages.java.profile.java.util.JavaResourceImpl
+ * @generated
+ */
+public class JavaResourceFactoryImpl extends ResourceFactoryImpl {
+	/**
+	 * Creates an instance of the resource factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public JavaResourceFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Resource createResource(URI uri) {
+		XMLResource result = new JavaResourceImpl(uri);
+		return result;
+	}
+
+} //JavaResourceFactoryImpl
